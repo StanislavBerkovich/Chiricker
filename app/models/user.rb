44180613aee9c00e3c07fd7680f85c031c123e48
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-06-13 16:36:25 +0300

    исправили костыль с дефолтными авами

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
-  has_and_belongs_to_many :writers, class_name: 'User'
-  has_and_belongs_to_many :readers, class_name: 'User'
 
 
-  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "no_avatar.png"
+  has_attached_file :avatar, :styles => {:medium => "300x300>", :small => "100x100>"}, :default_url => "no_avatar.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
