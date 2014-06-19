6315017d65128e6dc56522dfb0709ebdedb050f0
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-06-20 00:10:08 +0300

    При регистрации все переводится в нижний регистр

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,9 +15,19 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :surname, :nic, :city, :email, :password, :password_confirmation, :avatar)
+    downcase_params params.require(:user).permit(:name, :surname, :nic, :city, :email, :password, :password_confirmation, :avatar)
   end
+
+  def downcase_params(p)
+    p.each do |key, v|
+      if v.respond_to? :downcase
+        p[key] = v.downcase
+      end
+    end
+    p
+  end
+
   def account_update_params
-    params.require(:user).permit(:name, :surname, :nic, :city, :email, :password, :password_confirmation, :current_password, :avatar)
+    downcase_params  params.require(:user).permit(:name, :surname, :nic, :city, :email, :password, :password_confirmation, :current_password, :avatar)
   end
 end
